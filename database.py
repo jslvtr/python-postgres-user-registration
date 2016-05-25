ee5b454ffc8263aa1ccc4abea856ab80b501e091
Jose Salvatierra <jose@schoolofcode.me>
2016-05-25 11:02:58 +0100

    Changed the database file to contain a CursorFromConnectionPool class, which allows us to directly borrow a connection from the pool and get a cursor for it. This does not allow us to get the connection, but if all we want is a cursor, then this method is simpler. If we wanted to do other stuff with the connection (such as rolling back in the event of an error) we can no longer do this (although the __exit__ method can roll back if there is an exception).

--- database.py
+++ database.py
@@ -3,14 +3,17 @@ from psycopg2 import pool
 connection_pool = pool.SimpleConnectionPool(1, 10, database="learning", user="postgres", password="1234", host="localhost")
 # connection_pool = pool.SimpleConnectionPool(1, 1, database="learning", user="postgres", password="1234", host="localhost")
 
-class ConnectionFromPool:
+class CursorFromConnectionPool:
     def __init__(self):
         self.conn = None
+        self.cursor = None
 
     def __enter__(self):
         self.conn = connection_pool.getconn()
-        return self.conn
+        self.cursor = self.conn.cursor()
+        return self.cursor
 
     def __exit__(self, exception_type, exception_value, exception_traceback):
+        self.cursor.close()
         self.conn.commit()
         connection_pool.putconn(self.conn)
--- user.py
+++ user.py
@@ -1,4 +1,4 @@
-from database import ConnectionFromPool
+from database import CursorFromConnectionPool
 
 class User:
     def __init__(self, email, first_name, last_name, id=None):
@@ -12,16 +12,14 @@ class User:
 
     def save_to_db(self):
         # This is creating a new connection pool every time! Very expensive...
-        with ConnectionFromPool() as conn:
-            with conn.cursor() as cursor:
-                cursor.execute('INSERT INTO users (email, first_name, last_name) VALUES (%s, %s, %s)',
-                                (self.email, self.first_name, self.last_name))
+        with CursorFromConnectionPool() as cursor:
+            cursor.execute('INSERT INTO users (email, first_name, last_name) VALUES (%s, %s, %s)',
+                            (self.email, self.first_name, self.last_name))
 
     @classmethod
     def load_from_db_by_email(cls, email):
-        with ConnectionFromPool() as conn:
-            with conn.cursor() as cursor:
-                # Note the (email,) to make it a tuple!
-                cursor.execute('SELECT * FROM users WHERE email=%s', (email,))
-                user_data = cursor.fetchone()
-                return cls(email=user_data[1], first_name=user_data[2], last_name=user_data[3], id=user_data[0])
+        with CursorFromConnectionPool() as cursor:
+            # Note the (email,) to make it a tuple!
+            cursor.execute('SELECT * FROM users WHERE email=%s', (email,))
+            user_data = cursor.fetchone()
+            return cls(email=user_data[1], first_name=user_data[2], last_name=user_data[3], id=user_data[0])
